bd4168236461ddfd84ed849a753d0c0353a84eaa
Abdulwahab Adamson <adamsonoladipupo@gmail.com>
2025-10-17 21:48:09 +0100

    new update

---
+++ Snacks 17_10_2025/WorldPopulationGrowth.java
@@ -0,0 +1,32 @@
+public class WorldPopulationGrowth{
+	public static void main(String[] args){
+		String docString = """
+
+		Welcome!, This table displays the world polulation 
+		growth ( 75 years) using this formula:
+
+		P(t) = Po . e^rt
+
+		where:
+		- P(t) = population at time t
+		- Po = initial population (8230000000 (mid-2025))
+		- r = growth rate (0.84% == 0.0084)
+		- t = time (in year)
+		- e = Euler's number (2.71828)
+
+		""";
+		System.out.print(docString);
+
+		double population = 0;
+		double initial = 8.23;
+		double increasePolulation = 0;
+
+		System.out.println("Year	Anticipated Population	Increase Population");
+		for (int count = 1; count <=75; count++){
+			population = 8.23 * Math.pow(2.71828, (0.0084*count));
+			increasePolulation = population - initial;
+			System.out.printf("%d	%.2fB			%.2fB %n",count, population, increasePolulation);
+		}
+	}
+
+}
---
+++ Snacks 17_10_2025/privacyWithCryptography.js
@@ -0,0 +1,13 @@
+const prompt = require("prompt-sync")({sigint: true});
+
+let userInput = prompt("Send a simple message in four digits: ");
+
+		let[] message = new let [4];
+		for (let count = 0; count <=3; count++){
+
+			message[count] = prompt(f"Enter digit %d : ", count")
+		}
+		System.out.print("You entered: ");
+		for (let count = 0; count <=3; count++){
+			System.out.print(message[count]);
+		}
